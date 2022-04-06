3b12a6e45e094e90cda02c87379965df3f3ba521
mgmmchr123 <mgmmchr123@gmail.com>
2022-04-06 05:27:18 -0400

    Delete src/main/jooleproject/Repository directory

--- src/main/jooleproject/Repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itlizeSession.jooleproject.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.itlizeSession.jooleproject.Entity.Project;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-
-}
